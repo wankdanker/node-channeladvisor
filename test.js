cc5d15eb042bbb95daf06e422cac16c3ceca0b41
Dan VerWeire <dverweire@gmail.com>
2014-07-07 16:21:54 -0400

    Change test to use factory function and use asserts

--- test.js
+++ test.js
@@ -1,11 +1,14 @@
 var ChannelAdvisor = require('./');
+var assert = require('assert');
 
-var api = new ChannelAdvisor({
+var api = ChannelAdvisor({
 	developerKey : process.env.CHANNEL_ADVISOR_DEVELOPER_KEY
 	, password : process.env.CHANNEL_ADVISOR_PASSWORD
 });
 
-[
+var pending = 0;
+var completed = 0;
+var services = [
 	'AdminService'
 	, 'CartService'
 	, 'FulfillmentService'
@@ -15,8 +18,23 @@ var api = new ChannelAdvisor({
 	, 'OrderService'
 	, 'ShippingService'
 	, 'StoreService'
-].forEach(function (service) {
+];
+
+services.forEach(function (service) {
+	pending += 1;
 	api[service].Ping(function (err, data) {
+		pending -= 1;
+		completed +=1;
+
 		console.log(service, err, data);
+
+		assert.equal(err, null, 'Error should be null');
+		assert.deepEqual(data, { Status: 'Success', MessageCode: 0, ResultData: 'OK' }, 'Response data not OK');
+
+		if (pending === 0) {
+			assert.equal(completed, services.length, 'Not all tests finished');
+
+			console.log('Done');
+		}
 	});
 });
